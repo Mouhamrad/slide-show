13bf140b59a2443e8a3f4afffe6f5e713d794d66
mouhamedMrad <mouhamed.mrad@oyez.fr>
2020-07-15 14:48:21 +0200

    slidShow first attempt

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Slides({slides}) {
+    const [currSlide, setCurdrSlide] = useState(0);
+    const [nextButton, setNextButton] = useState(false);
+    const [prevButton, setPrevButton] = useState(false);
+    const [resButton, setRestartButton] = useState(false);
+    const [initStep, setnitStep] = useState(true);
+
+    useEffect(() => {
+
+        if (currSlide === 0) {
+            setRestartButton(true)
+        } else {
+            setRestartButton(false)
+        }
+        if (!nextButton && currSlide === (slides.length - 1) && !initStep) {
+            setNextButton(true)
+        } else if (nextButton && currSlide !== (slides.length - 1)) {
+            setNextButton(false)
+        }
+
+        if (!prevButton && currSlide === 0) {
+            setPrevButton(true)
+        } else if (prevButton && currSlide > 0) {
+            setPrevButton(false)
+        }
+
+        if (initStep && currSlide !== 0) {
+            setnitStep(false);
+        }
+    }, [currSlide]);
+
+    const handleResetButton = () => {
+        if (currSlide !== 0) {
+            setCurdrSlide(0)
+        }
+    };
+
+    const handlePrevButton = () => {
+        if (currSlide > 0) {
+            setCurdrSlide(currSlide - 1)
+        } else {
+            setPrevButton(true)
+        }
+    };
+
+    const handleNextButton = () => {
+        if (!nextButton && currSlide < (slides.length - 1)) {
+            setCurdrSlide(currSlide + 1)
+        } else {
+            setNextButton(true)
+        }
+    };
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={() => handleResetButton()}
+                        disabled={resButton}>Restart
+                </button>
+                <button data-testid="button-prev" className="small" onClick={() => handlePrevButton()}
+                        disabled={prevButton}>Prev
+                </button>
+                <button data-testid="button-next" className="small" onClick={() => handleNextButton()}
+                        disabled={nextButton}>Next
+                </button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[currSlide].title}</h1>
+                <p data-testid="text">{slides[currSlide].text}</p>
             </div>
         </div>
     );
-
 }
 
 export default Slides;
